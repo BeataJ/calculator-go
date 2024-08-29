e3519732d3264af9546a72469347af3c0cbfc11a
Beata <beata@jasinski.com>
2024-08-28 19:32:40 -0700

    add err

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	// "fmt"
+	"fmt"
 
-	"example.com/calculator/cmdmanager"
-	// "example.com/calculator/filemanager"
+	// "example.com/calculator/cmdmanager"
+	"example.com/calculator/filemanager"
 	"example.com/calculator/prices"
 )
 
@@ -12,10 +12,15 @@ func main() {
 
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 	for _, taxRate := range taxRates {
-		// fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
-		cmdm := cmdmanager.New()
-		priceJob := prices.NewTaxIncludedPriceJob(cmdm, taxRate)
-		priceJob.Process()
+		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		// cmdm := cmdmanager.New()
+		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
+		err := priceJob.Process()
+
+		if err != nil {
+			fmt.Println("could not process job")
+			fmt.Println(err)
+		}
 	}
 
 }
